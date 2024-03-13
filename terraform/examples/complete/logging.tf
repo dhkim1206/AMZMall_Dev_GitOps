a5fa3c172e3f652a6293636ae35b3fa566330df3
DOHYUNG <69470424+dhkim1206@users.noreply.github.com>
2024-03-13 12:12:02 +0900

    Refactor : Logging.tf

--- terraform/examples/complete/logging.tf
+++ terraform/examples/complete/logging.tf
@@ -1,42 +1,41 @@
 # C:\Users\user\Documents\GitHub\AMZMall_Dev_GitOps\terraform\examples\complete\logging.tf
-# IAM Policy for Fluent Bit to send logs to CloudWatch
+# IAM 정책 문서 정의: Fluent Bit가 AWS CloudWatch Logs에 로그를 보낼 때 필요한 권한 부여
+data "aws_iam_policy_document" "fluent_bit_cloudwatch_policy_doc" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:DescribeLogGroups"
+    ]
+    effect   = "Allow"
+    resources = ["*"]
+  }
+}
+
+# IAM 정책 생성: 정책 문서를 기반으로 실제 IAM 정책 리소스 생성
 resource "aws_iam_policy" "fluent_bit_cloudwatch_policy" {
   name        = "FluentBitCloudWatchPolicy-${var.cluster_name}"
   description = "Allow Fluent Bit to create and write to CloudWatch Logs"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogStreams",
-        "cloudwatch:PutMetricData",
-        "ec2:DescribeVolumes",
-        "ec2:DescribeTags",
-        "logs:DescribeLogGroups",
-      ],
-      Effect   = "Allow",
-      Resource = "*"
-    }]
-  })
+  policy      = data.aws_iam_policy_document.fluent_bit_cloudwatch_policy_doc.json
 }
 
-# IAM Role for the Fluent Bit Service Account
+# IAM 역할 생성: EKS 서비스 계정이 가정할 IAM 역할
 resource "aws_iam_role" "fluent_bit_service_account_role" {
   name = "FluentBitServiceAccountRole-${var.cluster_name}"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version   = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity",
-      Effect = "Allow",
+      Effect  = "Allow",
+      Action  = "sts:AssumeRoleWithWebIdentity",
       Principal = {
         Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"
       },
       Condition = {
         StringEquals = {
-          "${data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer}:sub": "system:serviceaccount:logging:fluent-bit-service-account"
+          "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:logging:fluent-bit-service-account"
         }
       }
     }]
@@ -47,20 +46,20 @@ resource "aws_iam_role" "fluent_bit_service_account_role" {
   }
 }
 
-# Attach the policy to the role
+# IAM 역할과 정책 연결
 resource "aws_iam_role_policy_attachment" "fluent_bit_cloudwatch_policy_attachment" {
   role       = aws_iam_role.fluent_bit_service_account_role.name
   policy_arn = aws_iam_policy.fluent_bit_cloudwatch_policy.arn
 }
 
-# Kubernetes Namespace for Logging
+# Kubernetes 네임스페이스 생성: 로깅용
 resource "kubernetes_namespace" "logging" {
   metadata {
     name = "logging"
   }
 }
 
-# Kubernetes Service Account for Fluent Bit
+# Kubernetes 서비스 계정 생성: Fluent Bit용, AWS IAM 역할과 연결됨
 resource "kubernetes_service_account" "fluent_bit_service_account" {
   metadata {
     name      = "fluent-bit-service-account"
@@ -71,7 +70,7 @@ resource "kubernetes_service_account" "fluent_bit_service_account" {
   }
 }
 
-# Deploy Fluent Bit using Helm
+# Helm을 사용하여 Fluent Bit 배포
 resource "helm_release" "fluent_bit" {
   name       = "fluent-bit"
   repository = "https://fluent.github.io/helm-charts"
