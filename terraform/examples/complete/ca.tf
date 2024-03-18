1f22fe5dfa7935a5fd85f04b13e7aaa0354a7e80
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2024-03-19 05:02:23 +0900

    Update - Monitoring

--- terraform/examples/complete/ca.tf
+++ terraform/examples/complete/ca.tf
@@ -1,92 +1,92 @@
-# C:\AMZMall_Dev_GitOps\terraform\examples\complete\ca.tf
-# CA IAM Role을 위한 정책 설정
-resource "aws_iam_policy" "ca_iam_policy" {
-  name        = "ca_iam_policy-${var.infra_name}"
-  description = "ca policy"
-  policy      = file("${path.module}/policy/ca_iam_policy.json")
-}
+# # C:\AMZMall_Dev_GitOps\terraform\examples\complete\ca.tf
+# # CA IAM Role을 위한 정책 설정
+# resource "aws_iam_policy" "ca_iam_policy" {
+#   name        = "ca_iam_policy-${var.infra_name}"
+#   description = "ca policy"
+#   policy      = file("${path.module}/policy/ca_iam_policy.json")
+# }
 
-data "aws_iam_policy_document" "ca_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect = "Allow"
-    principals {
-      type = "Federated"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"]
-    }
-    condition {
-      test = "StringEquals"
-      variable = "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub"
-      values = ["system:serviceaccount:kube-system:cluster-autoscaler"]
-    }
-  }
-}
+# data "aws_iam_policy_document" "ca_assume_role_policy" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect = "Allow"
+#     principals {
+#       type = "Federated"
+#       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}"]
+#     }
+#     condition {
+#       test = "StringEquals"
+#       variable = "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub"
+#       values = ["system:serviceaccount:kube-system:cluster-autoscaler"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "ca_iam_role" {
-  name = "${var.cluster_name}-ca-role"
+# resource "aws_iam_role" "ca_iam_role" {
+#   name = "${var.cluster_name}-ca-role"
 
-  assume_role_policy = data.aws_iam_policy_document.ca_assume_role_policy.json
-
-
-  tags = {
-    "Name" = "${var.cluster_name}-ca-role"
-  }
-}
+#   assume_role_policy = data.aws_iam_policy_document.ca_assume_role_policy.json
 
 
+#   tags = {
+#     "Name" = "${var.cluster_name}-ca-role"
+#   }
+# }
 
-resource "aws_iam_role_policy_attachment" "ca_iam_policy_attach" {
-  role       = aws_iam_role.ca_iam_role.name
-  policy_arn = aws_iam_policy.ca_iam_policy.arn
-}
 
-# Metrics Server Helm Release
-resource "helm_release" "metrics_server" {
-  namespace  = "kube-system"
-  name       = "metrics-server"
-  chart      = "metrics-server"
-  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-}
 
-resource "kubernetes_service_account" "cluster_autoscaler" {
-  metadata {
-    name        = "cluster-autoscaler"
-    namespace   = "kube-system"
-    annotations = {"eks.amazonaws.com/role-arn" = aws_iam_role.ca_iam_role.arn}
-  }
-}
+# resource "aws_iam_role_policy_attachment" "ca_iam_policy_attach" {
+#   role       = aws_iam_role.ca_iam_role.name
+#   policy_arn = aws_iam_policy.ca_iam_policy.arn
+# }
 
-# Cluster Autoscaler Helm Release
-resource "helm_release" "cluster_autoscaler" {
-  name       = "cluster-autoscaler"
-  namespace  = "kube-system"
-  chart      = "cluster-autoscaler"
-  repository = "https://kubernetes.github.io/autoscaler"
+# # Metrics Server Helm Release
+# resource "helm_release" "metrics_server" {
+#   namespace  = "kube-system"
+#   name       = "metrics-server"
+#   chart      = "metrics-server"
+#   repository = "https://kubernetes-sigs.github.io/metrics-server/"
+# }
 
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = var.cluster_name
-  }
+# resource "kubernetes_service_account" "cluster_autoscaler" {
+#   metadata {
+#     name        = "cluster-autoscaler"
+#     namespace   = "kube-system"
+#     annotations = {"eks.amazonaws.com/role-arn" = aws_iam_role.ca_iam_role.arn}
+#   }
+# }
 
-  set {
-    name  = "awsRegion"
-    value = var.aws_region
-  }
+# # Cluster Autoscaler Helm Release
+# resource "helm_release" "cluster_autoscaler" {
+#   name       = "cluster-autoscaler"
+#   namespace  = "kube-system"
+#   chart      = "cluster-autoscaler"
+#   repository = "https://kubernetes.github.io/autoscaler"
 
-  set {
-    name  = "rbac.serviceAccount.create"
-    value = "false"
-  }
+#   set {
+#     name  = "autoDiscovery.clusterName"
+#     value = var.cluster_name
+#   }
 
-  set {
-    name  = "rbac.serviceAccount.name"
-    value = kubernetes_service_account.cluster_autoscaler.metadata[0].name
-  }
+#   set {
+#     name  = "awsRegion"
+#     value = var.aws_region
+#   }
 
-  set {
-    name  = "rbac.serviceAccount.annotations.eks.amazonaws.com/role-arn"
-    value = aws_iam_role.ca_iam_role.arn
-  }
+#   set {
+#     name  = "rbac.serviceAccount.create"
+#     value = "false"
+#   }
 
-  values = [file("${path.module}/values/ca-values.yaml")]
-}
+#   set {
+#     name  = "rbac.serviceAccount.name"
+#     value = kubernetes_service_account.cluster_autoscaler.metadata[0].name
+#   }
+
+#   set {
+#     name  = "rbac.serviceAccount.annotations.eks.amazonaws.com/role-arn"
+#     value = aws_iam_role.ca_iam_role.arn
+#   }
+
+#   values = [file("${path.module}/values/ca-values.yaml")]
+# }
--- terraform/examples/complete/monitoring.tf
+++ terraform/examples/complete/monitoring.tf
@@ -1,5 +1,43 @@
-# # C:\Users\user\Documents\GitHub\AMZMall_Dev_GitOps\terraform\examples\complete\monitoring.
+# C:\AMZMall_Dev_GitOps\terraform\examples\complete\monitoring.tf
 
+locals {
+  oidc_provider = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
+}
+
+resource "aws_iam_policy" "loki_iam_policy" {
+  name        = "AWSS3EksLokiAccess-${var.infra_name}"
+  description = "Policy for Loki to access S3"
+  policy      = file("${path.module}/policy/loki_iam_policy.json")
+}
+
+resource "aws_iam_role" "loki_iam_role" {
+  name               = "AmazonEKS-Loki-Role-${var.infra_name}"
+  assume_role_policy = data.aws_iam_policy_document.loki_assume_role_policy.json
+  tags = {
+    "Name" = "AmazonEKS-Loki-Role-${var.infra_name}"
+  }
+}
+
+data "aws_iam_policy_document" "loki_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider}"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${local.oidc_provider}:sub"
+      values   = ["system:serviceaccount:monitoring:loki-sa"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "loki_iam_policy_attachment" {
+  role       = aws_iam_role.loki_iam_role.name
+  policy_arn = aws_iam_policy.loki_iam_policy.arn
+}
 
 resource "kubernetes_namespace" "monitoring" {
   metadata {
@@ -7,12 +45,29 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-# prometheus, grafana 설치
-resource "helm_release" "kube-prometheus-stack" {
-  name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace  = "monitoring"
-
+resource "kubernetes_service_account" "loki_service_account" {
+  metadata {
+    name        = "loki-sa"
+    namespace   = "monitoring"
+    annotations = {"eks.amazonaws.com/role-arn" = aws_iam_role.loki_iam_role.arn}
+  }
 }
 
+resource "helm_release" "loki" {
+  name       = "loki"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "loki-distributed"
+  namespace  = "monitoring"
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.loki_service_account.metadata[0].name
+  }
+
+  values = [file("${path.module}/values/loki-values.yaml")]
+}
